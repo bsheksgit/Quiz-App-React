df81af23f6777b2b4829f47e0e18278c0ecbdb7a
Abhishek A B <bsheks@gmail.com>
2025-10-25 01:00:33 +0530

    Added image and basic box

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
-function App() {}
+import logo from "./assets/quiz-logo.png";
+
+function App() {
+    return (
+        <>
+        <header>
+        <img src={logo} alt="Logo" />
+        <h1>React Quiz</h1>
+        </header>
+        <div id="quiz">
+        <div id ="question">
+
+        </div>
+        </div>
+        </>
+    );
+}
 
 export default App;
