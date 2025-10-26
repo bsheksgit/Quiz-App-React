213d1981561ee9fd645469dcf195461a5ba84c1e
Abhishek A B <bsheks@gmail.com>
2025-10-26 20:42:29 +0530

    Added Context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import logo from "./assets/quiz-logo.png";
 import QuizQuestion from "./components/QuizQuestion";
 import Summary from "./components/Summary";
+import QuizContextProvider, { QuizContext } from "./store/quiz-app-context";
 
 function App() {
     return (
         <>
+        <QuizContextProvider>
         <header>
         <img src={logo} alt="Logo" />
         <h1>React Quiz</h1>
         </header>
-        <Summary />
+        <QuizQuestion />
+        </QuizContextProvider>
         </>
     );
 }
--- src/components/QuizQuestion.jsx
+++ src/components/QuizQuestion.jsx
@@ -1,17 +1,22 @@
-        
+import { QuizContext } from "../store/quiz-app-context";
+import { useContext } from "react";
+
 export default function QuizQuestion(){
+
+        const quizContext = useContext(QuizContext);
+
         return(
         <>
         <section id="quiz">
         <div id ="question">
             <progress value={40} max={100} />
-            <h2>What is JSX?</h2>
+            <h2>{quizContext.questionOptions[0].text}</h2>
         </div>
         <ul id="answers">
-        <li class="answer"><button>A. useState</button></li>
-        <li class="answer"><button>B. useEffect</button></li>
-        <li class="answer"><button>C. useMemo</button></li>
-        <li class="answer"><button>D. useRef</button></li>
+        <li className="answer"><button className="selected">A. useState</button></li>
+        <li className="answer"><button>B. useEffect</button></li>
+        <li className="answer"><button>C. useMemo</button></li>
+        <li className="answer"><button>D. useRef</button></li>
         </ul>
         </section>
         </>
