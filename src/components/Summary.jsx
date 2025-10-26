245eaf35c450ea5b0cb40236d7519ffc251e67ad
Abhishek A B <bsheks@gmail.com>
2025-10-26 19:14:34 +0530

    Completed the basic layout of both components

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -6,6 +6,28 @@ export default function Summary(){
             <section id="summary">
             <img src={quizComplete}/>
             <h2>Quiz Completed!</h2>
+            <div id="summary-stats">
+            <div>
+            <p className="number">100%</p>
+            <p className="text">Skipped</p>
+            </div>
+            <div>
+            <p className="number">100%</p>
+            <p className="text">Skipped</p>
+            </div>
+            <div>
+            <p className="number">100%</p>
+            <p className="text">Skipped</p>
+            </div>
+            </div>
+            <ol>
+            <li>
+                <h3>1</h3>
+                <p className="question">What is React?</p>
+                <p className="user-answer correct">React is a UI building tool.</p>
+            </li>
+
+            </ol>
             </section>
             </>
         );
